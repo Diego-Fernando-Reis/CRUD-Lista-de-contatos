343fc5e8e8da5082eb86222d908978004ce057de
unknown <diegoreis.dvlopr@gmail.com>
2022-09-21 17:35:19 -0300

    estilização das páginas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import About from './pages/About';
 import AddEdit from './pages/AddEdit';
 import Home from './pages/Home';
 import View from './pages/View';
+import Header from './components/Header'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/' component={AddEdit} />
-          <Route path='/' component={AddEdit} />
-          <Route path='/' component={View} />
-          <Route path='/' component={About} />
-        </Switch>
+        <Header />
+        <ToastContainer position='top-center'/>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/add' element={<AddEdit />} />
+          <Route path='/update/:id' element={<AddEdit />} />
+          <Route path='/view/:id' element={<View />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
