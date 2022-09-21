0ef21c3a2a2926fd9e1aee05b522455a488ce916
unknown <diegoreis.dvlopr@gmail.com>
2022-09-21 15:14:37 -0300

    adicionando paginas e rotas no aplicativo

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ3Fwbz6fJWgLWYtvY5-GxCwegIV58m0k",
+  authDomain: "app-contato-25d4b.firebaseapp.com",
+  projectId: "app-contato-25d4b",
+  storageBucket: "app-contato-25d4b.appspot.com",
+  messagingSenderId: "633825068634",
+  appId: "1:633825068634:web:047fa18010f3da6b69ba63"
+};
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import AddEdit from './pages/AddEdit';
+import Home from './pages/Home';
+import View from './pages/View';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/' component={AddEdit} />
+          <Route path='/' component={AddEdit} />
+          <Route path='/' component={View} />
+          <Route path='/' component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
